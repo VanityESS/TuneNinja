0cadec396848ae659116142e03233c73c6f5c0b9
VanityESS <VanityESS@gmail.com>
2023-11-22 15:44:53 +0100

    Get access token works

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,41 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="getToken">Get Token</button>
+    <button @click="getArtistInfo">Get Artist Info</button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
+let accessToken='';
 
+export default {
+  methods: {
+    async getToken() {
+      const url = 'https://accounts.spotify.com/api/token';
+      const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      };
+
+      const data = new URLSearchParams();
+      data.append('grant_type', 'client_credentials');
+      data.append('client_id', '30a9caca1b0549dd91e747767a6879fc');
+      data.append('client_secret', 'c4611d2c7da84c85b82d6611b5ad98ac');
+
+      try {
+        const response = await axios.post(url, data, { headers });
+        // Access the access token from the response object
+         accessToken = response.data.access_token;
+        console.log('Access Token:', accessToken);
+        // Perform further actions with the access token if needed
+      } catch (error) {
+        console.error('Error getting token:', error);
+      }
+    }
+    
+  }
+};
+</script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
